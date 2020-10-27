3c57791b502c180249f7141e6cbce0a0df911ac7
Wei Tao <wtao7@fordham.edu>
2020-10-27 01:16:07 -0400

    Add Material-UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import SignIn from './components/TestPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SignIn />
     </div>
   );
 }
