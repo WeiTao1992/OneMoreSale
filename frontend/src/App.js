c7e728d714d685e8a6efb79612130574558fbfaf
Wei Tao <wtao7@fordham.edu>
2020-10-27 02:43:19 -0400

    Add React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import OneMoreSale from './components/OneMoreSale';
 import SignIn from './components/TestPage';
 
 function App() {
   return (
     <div className="App">
+      <OneMoreSale />
       <SignIn />
     </div>
   );
