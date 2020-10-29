6f7b1bdbb498713bf13ced3c9189c41c84c60a5d
xsmming <jing.mi@northeastern.edu>
2020-10-29 15:57:08 -0400

    spring boot initilizer

---
+++ backend/OneMoreSale/src/main/java/com/onemoresale/OneMoreSale/OneMoreSaleApplication.java
@@ -0,0 +1,13 @@
+package com.onemoresale.OneMoreSale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneMoreSaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneMoreSaleApplication.class, args);
+	}
+
+}
---
+++ backend/OneMoreSale/src/test/java/com/onemoresale/OneMoreSale/OneMoreSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onemoresale.OneMoreSale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneMoreSaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
