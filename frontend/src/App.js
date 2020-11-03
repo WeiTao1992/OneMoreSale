4b8ed8f058c950231c757781932a59eb462e5f9d
Wei Tao <wtao7@fordham.edu>
2020-11-02 22:23:00 -0500

    top bar building

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import TopBar from "./topbar/TopBar";
 import OneMoreSale from './OneMoreSale';
 
 function App() {
   return (
     <div className="App">
+      <TopBar position = "sticky" />
       <OneMoreSale />
     </div>
   );
