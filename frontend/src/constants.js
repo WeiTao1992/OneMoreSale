efdd3f4e4cf001c4dc983c11f62fec524c6b6c37
stinkycc <65824755+stinkycc@users.noreply.github.com>
2020-11-04 15:23:42 -0800

    q

--- frontend/src/constants.js
+++
@@ -1,1 +0,0 @@
-export const API_ROOT = 'https://my-json-server.typicode.com/stinkycc/SHMTest';
--- frontend/src/item/Item.js
+++ frontend/src/item/Item.js
@@ -1,29 +1,16 @@
 import React, { Component } from 'react'
-import ReactBnbGallery from 'react-bnb-gallery'
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Divider from '@material-ui/core/Divider';
 import Link from '@material-ui/core/Link';
 import Container from '@material-ui/core/Container';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import {API_ROOT} from "../constants";
 import {useState, useEffect} from 'react';
 import Typography from '@material-ui/core/Typography';
-import ImageGallery from 'react-image-gallery';
+import Carousel from 'nuka-carousel';
+//import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
-const images = [{
-    original: "https://source.unsplash.com/aZjw7xI3QAA/1144x763",
-    thumbnail: "https://source.unsplash.com/aZjw7xI3QAA/100x67",
-  }, {
-    original: "https://source.unsplash.com/c77MgFOt7e0/1144x763",
-    thumbnail: "https://source.unsplash.com/c77MgFOt7e0/100x67",
-  }, {
-    original: "https://source.unsplash.com/QdBHnkBdu4g/1144x763",
-    thumbnail: "https://source.unsplash.com/QdBHnkBdu4g/100x67",
-  }];
-
+const API_ROOT = 'https://my-json-server.typicode.com/stinkycc/SHMTest'
 const useStyles = makeStyles((theme) => ({
     root: {
         width: '100%',
@@ -31,6 +18,8 @@ const useStyles = makeStyles((theme) => ({
         flexGrow:1,
     },
     gridItem:{
+        marginTop:3,
+        marginBottom:1,
         width:'50%',
     },
     gridList: {
@@ -89,6 +78,8 @@ export default function Item() {
         getItems(setPost);
         getImgs(setImgs);
     });
+
+    
     return (
         <Container component="main" maxWidth="l">
             <div >
@@ -104,26 +95,19 @@ export default function Item() {
                     </Link>
                 </Grid>
                 <Divider/>
-                <br></br>
                 <Grid container spacing={space} >
-                    <Grid item className={classes.gridItem} spacing={space}>
-                        {/* <ImageGallery items={images} /> */}
-                        <GridList className={classes.gridList} cols={2.5}>
-                        
-                            {
-                                imgs.map((tile) => (
-                                    <GridListTile key={tile.img}>
-                                        <img src={tile.img}/>
-                                        <GridListTileBar
-                                            classes={{
-                                                root: classes.titleBar,
-                                                title: classes.title,
-                                            }}
-
-                                        />
-                                    </GridListTile>
-                                ))}
-                        </GridList>
+                    <Grid item className={classes.gridItem} spacing={space} alignItems="center">
+                    {/* <AliceCarousel >
+                        <img src="https://source.unsplash.com/aZjw7xI3QAA/1144x763" />
+                        <img src="https://source.unsplash.com/c77MgFOt7e0/1144x763" />
+                        <img src="https://source.unsplash.com/QdBHnkBdu4g/1144x763" />
+                    </AliceCarousel>  */}
+                    <Carousel>
+                    {
+                        imgs.map((tile) => (
+                            <img src={tile.img}/>
+                    ))}
+                    </Carousel>
                     </Grid>
                     <Grid item className={classes.gridItem} >
                         <Typography gutterBottom variant="h6" align="left">{post.title}</Typography>
@@ -138,21 +122,18 @@ export default function Item() {
                             </Grid>
                         </Grid>
                         <Divider/>
-                        <br></br>
                         <Grid item className={classes.gridItem} spacing={space}>
                             <Typography color="textSecondary" variant="body2" align="left">Condition: {post.condition}</Typography>
-                            <Typography color="textSecondary" variant="body2" align="left">Transaction: cash</Typography>
-                            <Typography color="textSecondary" variant="body2" align="left">Delivery typ: pick up</Typography>
+                            <Typography color="textSecondary" variant="body2" align="left">Transaction: &#9737; cash</Typography>
+                            <Typography color="textSecondary" variant="body2" align="left">Delivery typ: &#9737; pick up &#9737; mail</Typography>
                         </Grid>
                         <Divider/>
-                        <br></br>
                         <Grid item className={classes.gridItem} spacing={space}>
-                            <Typography color="textSecondary" variant="body2" align="left">Description:{post.description}</Typography>
+                            <Typography color="textSecondary" variant="body2" align="left">Description: {post.description}</Typography>
                         </Grid>
                     </Grid>
                 </Grid>
                 <Divider/>
-                <br></br>
                 <Grid container spacing={space}>
                     <Grid item className={classes.gridItem} spacing={space}>
                         <Typography gutterBottom variant="h6" align="left">Contact Info</Typography>
@@ -161,13 +142,12 @@ export default function Item() {
                         <Typography color="textSecondary" variant="body2" align="left">Loaction: {post.address} ZipCode:{post.zipcodes}</Typography>
                     </Grid>
                     <Grid item className={classes.gridItem} spacing={space}>
-                        <div> map </div>
-                        
+                   
+         
                     </Grid>
                 </Grid>
             
-
-
+    
             </div>
 
         </Container>
