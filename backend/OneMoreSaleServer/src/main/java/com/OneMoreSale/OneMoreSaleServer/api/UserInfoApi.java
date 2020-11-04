838e98c5de433fc172ac084cc5b9c4533ae701f9
XinheY <gotzeyxinhe@gmail.com>
2020-11-03 23:25:11 -0800

    Update User Profile API URL

--- backend/OneMoreSaleServer/src/main/java/com/OneMoreSale/OneMoreSaleServer/api/UserInfoApi.java
+++ backend/OneMoreSaleServer/src/main/java/com/OneMoreSale/OneMoreSaleServer/api/UserInfoApi.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/userinfo")
 public class UserInfoApi {
 
     @Autowired
