c9eaacea8ba99895f2e138ad208eb780de86ad3e
Wei Tao <wtao7@fordham.edu>
2020-11-05 19:42:21 -0500

    Delete useless files

--- node_modules/nuka-carousel/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = require('./lib').default;
-module.exports.default = module.exports;
-module.exports.NextButton = require('./lib').NextButton;
-module.exports.PreviousButton = require('./lib').PreviousButton;
-module.exports.PagingDots = require('./lib').PagingDots;
