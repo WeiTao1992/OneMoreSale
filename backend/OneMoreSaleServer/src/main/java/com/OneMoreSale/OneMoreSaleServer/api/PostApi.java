e7821dd592ba7312bb8640a598ef206247efa95c
Shize Ligit config --global user.name shize.li@sjsu.edu <54549527+lishize1234@users.noreply.github.com>
2020-11-05 20:20:36 -0800

    delete useless file

--- backend/OneMoreSaleServer/src/main/java/com/OneMoreSale/OneMoreSaleServer/api/PostApi.java
+++ backend/OneMoreSaleServer/src/main/java/com/OneMoreSale/OneMoreSaleServer/api/PostApi.java
@@ -1,12 +1,18 @@
 package com.OneMoreSale.OneMoreSaleServer.api;
 
+import com.OneMoreSale.OneMoreSaleServer.HttpUtil;
 import com.OneMoreSale.OneMoreSaleServer.model.Post;
+import com.OneMoreSale.OneMoreSaleServer.model.User;
 import com.OneMoreSale.OneMoreSaleServer.service.PostService;
+import com.OneMoreSale.OneMoreSaleServer.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -14,16 +20,30 @@ public class PostApi {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private UserInfoService userInfoService;
     @PostMapping("/post/createpost")
-    public void savePost(@RequestBody Post post,
-                         @RequestParam (value = "id") int userId){
-
-        postService.savePost(post);
+    public void savePost(@RequestBody @NonNull Post post, HttpServletRequest httpServletRequest,
+                         HttpServletResponse httpServletResponse) throws IOException {
+        if (!HttpUtil.sessionInvalid(httpServletRequest)){  // already logged in
+            int userId = (int)httpServletRequest.getSession().getAttribute("user_id");
+            User user = userInfoService.getUserById(userId);
+            post.setUser(user);
+            postService.savePost(post);
+            return;
+        }
+        httpServletResponse.getWriter().print("Please log in first");
     }
 
     @GetMapping("/user/getallposts")
-    public List<Post> getAllPost(@RequestParam (value = "id") int userId){
-        return postService.getAllPost(userId);
+    public List<Post> getAllPost(HttpServletRequest httpServletRequest,
+                                 HttpServletResponse httpServletResponse) throws IOException{
+        if (!HttpUtil.sessionInvalid(httpServletRequest)){ // already logged in
+            int userId = (int)httpServletRequest.getSession().getAttribute("user_id");
+            return postService.getAllPost(userId);
+        }
+        httpServletResponse.getWriter().print("Please log in first");
+        return null;
     }
 
     @GetMapping("/post/getpostbyid")
