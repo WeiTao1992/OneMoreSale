30598c5b52d76d38c3c39c52137ed312b6807b63
Wei Tao <wtao7@fordham.edu>
2020-11-08 22:35:53 -0500

    Add topbar React-Query

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { QueryCache, ReactQueryCacheProvider } from 'react-query';
+import defaultQueryFn from "./util/defaultQueryFn";
 import {BrowserRouter as Router} from "react-router-dom";
 import TopBar from "./topbar/TopBar";
 import OneMoreSale from './OneMoreSale';
 
+ // provide the default query function to your app with defaultConfig
+ const queryCache = new QueryCache({
+  defaultConfig: {
+    queries: {
+      queryFn: defaultQueryFn,
+    },
+  },
+})
+
 function App() {
   return (
     <div className="App">
-      <Router>
-        <TopBar position = "sticky" />
-        <OneMoreSale />
-      </Router>
+      <ReactQueryCacheProvider queryCache={queryCache}>
+        <Router>
+          <TopBar position = "sticky" />
+          <OneMoreSale />
+        </Router>
+      </ReactQueryCacheProvider>
     </div>
   );
 }
