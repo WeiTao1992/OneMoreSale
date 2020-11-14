64e39b6e833b23b6bc0bbc058403fa674cc5e53b
Wei Tao <wtao7@fordham.edu>
2020-11-13 21:53:21 -0500

    Update account link in topbar

---
+++ frontend/src/topbar/AccountController.js
@@ -0,0 +1,28 @@
+import { useQuery } from 'react-query';
+import { Link } from "react-router-dom";
+import defaultQueryFn from '../util/defaultQueryFn';
+
+export function AccountController() {
+    const { isLoading, isError, data } = useQuery(['username', 'userinfo/getUserInfo/'], defaultQueryFn);
+
+    if (isLoading) {
+        <Link>
+          My Account
+        </Link>
+    }
+
+    if (isError) {
+        console.log("error")
+        return ( 
+        <Link to="/login">
+          My Account
+        </Link>
+        )
+    }
+
+    return (
+    <Link to="/account">
+      My Account
+    </Link>
+    )
+}
