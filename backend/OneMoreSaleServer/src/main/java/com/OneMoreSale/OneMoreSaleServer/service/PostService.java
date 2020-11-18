e8e29f547a81dd59d07e6246ff4a97448fc4cfaf
xsmming <jing.mi@northeastern.edu>
2020-11-18 03:13:52 -0500

    cache v2 for posts

--- backend/OneMoreSaleServer/src/main/java/com/OneMoreSale/OneMoreSaleServer/service/PostService.java
+++ backend/OneMoreSaleServer/src/main/java/com/OneMoreSale/OneMoreSaleServer/service/PostService.java
@@ -2,10 +2,13 @@ package com.OneMoreSale.OneMoreSaleServer.service;
 
 import com.OneMoreSale.OneMoreSaleServer.Dao.PostDao;
 import com.OneMoreSale.OneMoreSaleServer.model.*;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class PostService {
@@ -13,7 +16,11 @@ public class PostService {
     @Autowired
     private PostDao postDao;
 
-    public int savePost(Post post){
+    @Autowired
+    private UserInfoService userInfoService;
+
+
+    public int savePost(User user, Post post){
 
 
         if (post.getTransactionMethod() != null){
@@ -33,13 +40,16 @@ public class PostService {
                 postImage.setPost(post);
             }
         }
-        return postDao.savePost(post);
+        int id = postDao.savePost(post);
+        userInfoService.addPostToUserCache(user, post);
+        return id;
     }
 
-    public void deletePost(int postId){
-
-        postDao.deletePost(postId);
-
+    public void deletePost(User user, int postId){
+        Post post = postDao.deletePost(postId);
+        if (post != null) { // update cache
+            userInfoService.removePostFromUserCache(user, post);
+        }
     }
 
     public void editPost(Post post){
